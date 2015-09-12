b0aedad4c62776bed82570e4b0dca37e2393bbac
johan hosk <johan@Johans-MacBook-Pro.local>
2015-09-12 13:17:42 +0200

    Initial Commit

---
+++ netTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  netTrack
+//
+//  Created by Johan on 12/09/15.
+//  Copyright (c) 2015 Johan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
