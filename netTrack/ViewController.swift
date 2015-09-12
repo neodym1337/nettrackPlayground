72a07cae3e0b16ecd64953b568bb9f05438b1d87
johan hosk <johan@Johans-MacBook-Pro.local>
2015-09-12 14:11:00 +0200

    publish location

--- netTrack/ViewController.swift
+++ netTrack/ViewController.swift
@@ -12,7 +12,7 @@ import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, CLLocationManagerDelegate {
     
-    let username = "Johan"
+    let username = "Johan" //Set your own username here
     let mqttUsername = "playground"
     let mqttPassword = "edge"
     let mqttTopic = "netlight"
@@ -28,33 +28,67 @@ class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.showsUserLocation = true
+        
+        //Setup mqtt and connect
         mqttSession = MQTTSession(clientId: deviceID, userName: mqttUsername, password: mqttPassword)
         mqttSession.delegate = self
         mqttSession.connectToHost(mqttHostname, port: UInt32(mqttPort))
         
         locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //Best accuracy for tracking indoors
+        locationManager.distanceFilter = 1.0 //Filter location update difference 1 m
+        locationManager.requestAlwaysAuthorization() //Prompt user to use device location
+    }
+    
+    //MARK: CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = manager.location
+        let coordinate = location.coordinate
+        
+        println("Updated location: lat \(coordinate.latitude) lng \(coordinate.longitude)")
+        publishLocation(location)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        println("didChangeAuthorizationStatus")
+        switch (status) {
+        case CLAuthorizationStatus.NotDetermined:
+            println(".NotDetermined")
+            break
+        case CLAuthorizationStatus.AuthorizedAlways, CLAuthorizationStatus.AuthorizedWhenInUse:
+            println(".Authorized")
+            locationManager.startUpdatingLocation()
+            break
+        case .Denied:
+            println(".Denied")
+            break
+        default:
+            println("Unhandled authorization status")
+            break
+        }
     }
 
-    func publishLocation(coordinate: CLLocationCoordinate2D) {
+    //MARK: MQTT Publish
+    func publishLocation(location: CLLocation) {
+        
+        let coordinate = location.coordinate
         let payload = "{\"deviceID\":\"\(deviceID)\",\"name\":\"\(username)\",\"lat\":\(coordinate.latitude),\"lng\":\(coordinate.longitude)}"
         
         let data = payload.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
         mqttSession.publishData(data, onTopic: mqttTopic)
     }
     
-    //MARK: MQTT Message received
+    //MARK: MQTTSessionDelegate
     func newMessage(session: MQTTSession!, data: NSData!, onTopic topic: String!, qos: MQTTQosLevel, retained: Bool, mid: UInt32) {
         println("Message received")
     }
-    
     
     func handleEvent(session: MQTTSession!, event eventCode: MQTTSessionEvent, error: NSError!) {
         switch (eventCode) {
         case MQTTSessionEvent.Connected:
             println("Connected")
-            mqttSession.subscribeTopic(mqttTopic)
-            
+            mqttSession.subscribeTopic(mqttTopic) //Subscribe to listen after we are connected
             break
         case MQTTSessionEvent.ProtocolError, .ConnectionClosedByBroker, .ConnectionError, .ConnectionRefused:
             println("Connection error")
@@ -62,15 +96,11 @@ class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, 
         case MQTTSessionEvent.ConnectionClosed:
             println("Connection closed")
             break
-
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
-
 }
 
