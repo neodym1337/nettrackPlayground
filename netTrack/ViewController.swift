68e81d7fb5326f83d7c54b6fc40041bb00641b0a
johan hosk <johan@Johans-MacBook-Pro.local>
2015-09-14 21:32:33 +0200

    removed code

--- netTrack/ViewController.swift
+++ netTrack/ViewController.swift
@@ -9,11 +9,10 @@
 import UIKit
 import MapKit
 import CoreLocation
-//import SwiftyJSON
 
 class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, CLLocationManagerDelegate {
     
-    let username = "Johan" //Set your own username here
+    let username = "xxxxx" //Set your own username here
     let mqttUsername = "playground"
     let mqttPassword = "edge"
     let mqttTopic = "netlight"
@@ -22,40 +21,23 @@ class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, 
     var mqttSession : MQTTSession!
     let userDeviceID = UIDevice.currentDevice().identifierForVendor.UUIDString
     
-    var trackedUsers = [String: UserAnnotation]() //Create dictonary to store annotations for tracked users
-    
-    @IBOutlet weak var mapView: MKMapView!
-    
-    let locationManager = CLLocationManager()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.showsUserLocation = true
-        setupMqtt()
-        setupLocationManager()
     }
     
-    func setupMqtt() {
-        mqttSession = MQTTSession(clientId: userDeviceID, userName: mqttUsername, password: mqttPassword)
-        mqttSession.delegate = self
-        mqttSession.connectToHost(mqttHostname, port: mqttPort)
+    func setupAndConnectToMqtt() {
+        
     }
     
     func setupLocationManager() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest //Best accuracy for tracking indoors
-        locationManager.distanceFilter = 1.0 //Filter location update difference 1 m
-        locationManager.requestAlwaysAuthorization() //Prompt user to use device location
+        
+        //Setup and request auth
+
     }
     
     //MARK: CLLocationManagerDelegate
     
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        let location = manager.location //Take last known location
-        let coordinate = location.coordinate
-        println("Updated location: lat \(coordinate.latitude) lng \(coordinate.longitude)")
-        publishLocation(coordinate)
-    }
+    //---- Add CLLocationManager delegate methods here
     
     func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         println("didChangeAuthorizationStatus")
@@ -65,7 +47,6 @@ class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, 
             break
         case CLAuthorizationStatus.AuthorizedAlways, CLAuthorizationStatus.AuthorizedWhenInUse:
             println(".Authorized")
-            locationManager.startUpdatingLocation() //Once authorized, start updating locations
             break
         case .Denied:
             println(".Denied")
@@ -76,72 +57,20 @@ class ViewController: UIViewController, MKMapViewDelegate, MQTTSessionDelegate, 
         }
     }
 
-    //MARK: MQTT Publish
-    func publishLocation(coordinate: CLLocationCoordinate2D) {
-        let payloadData = createPayloadFromLocation(coordinate)
-        mqttSession.publishData(payloadData, onTopic: mqttTopic) //Publish encoded payload on our topic
-    }
-    
-    func createPayloadFromLocation(coordinate : CLLocationCoordinate2D) -> NSData {
+    //MARK: MQTT Helpers
+
+    /*
+    func createPayloadFromLocation(coordinate : CLLocationCoordinate2D)  {
         // Create payload json using our new location together with the data needed for other clients to identify us
         let payload = "{\"deviceID\":\"\(userDeviceID)\",\"name\":\"\(username)\",\"lat\":\(coordinate.latitude),\"lng\":\(coordinate.longitude)}"
-        return payload.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+        
+        //return nsdata from payload
     }
+    */
     
     //MARK: MQTTSessionDelegate
-    func newMessage(session: MQTTSession!, data: NSData!, onTopic topic: String!, qos: MQTTQosLevel, retained: Bool, mid: UInt32) {
-        
-        //OBS: Disregard message coming from self deviceID since we are publishing and subscribing to the same topic
-        let json = JSON(data: data)
-        if let receivedDeviceID = json["deviceID"].string  {
-            if receivedDeviceID == userDeviceID  {
-                //println("Message from self, do not use")
-                return;
-            }
-        }else {
-            println("Error parsing json")
-        }
-        
-        let newAnnotation = createAnnotationFromJson(json)
-        println("Message received from \(newAnnotation.title)")
-        
-        processAnnotation(newAnnotation)
-    }
     
-    func processAnnotation(annotation: UserAnnotation) {
-        if let previousUser = trackedUsers[annotation.deviceID] {
-            //If we have already added an annotation from this user to the map we just have to update the coordinate
-            previousUser.coordinate = annotation.coordinate // Set new coordinate
-        }else {
-            // If its the first time we receive a message from this user we should add it to the map
-            trackedUsers[annotation.deviceID] = annotation
-            self.mapView.addAnnotation(annotation)
-        }
-    }
-    
-    func createAnnotationFromJson(json : JSON) -> UserAnnotation {
-        let name = json["name"].string!
-        let deviceID = json["deviceID"].string!
-        let lat = json["lat"].double!
-        let lng = json["lng"].double!
-        let coordinate = CLLocationCoordinate2DMake(lat, lng)
-        return UserAnnotation(coordinate: coordinate, name: name, deviceID: deviceID)
-    }
-    
-    func handleEvent(session: MQTTSession!, event eventCode: MQTTSessionEvent, error: NSError!) {
-        switch (eventCode) {
-        case MQTTSessionEvent.Connected:
-            println("Connected")
-            mqttSession.subscribeTopic(mqttTopic) //Subscribe to listen after we are connected
-            break
-        case MQTTSessionEvent.ProtocolError, .ConnectionClosedByBroker, .ConnectionError, .ConnectionRefused:
-            println("Connection error")
-            break
-        case MQTTSessionEvent.ConnectionClosed:
-            println("Connection closed")
-            break
-        }
-    }
+    //----Add mqtt delegate methods
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
